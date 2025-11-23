b49963dc9d7b4220e59e608e923c4ee5cd78e2da
JoseRosario967 <joserosario@live.com.pt>
2025-11-23 00:48:18 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Carrega as variáveis de ambiente (como a API_KEY)
-  // O process.cwd() às vezes falha no TypeScript estrito, mas aqui vai funcionar
   const env = loadEnv(mode, process.cwd(), '');
-  
   return {
     plugins: [react()],
     define: {
-      // Passa a chave API para o navegador de forma segura
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
     },
     build: {
       outDir: 'dist',
       sourcemap: false,
-      // Ignora erros de TypeScript durante a construção para garantir o deploy
       typescript: {
-        ignoreBuildErrors: true,
-      },
+        ignoreBuildErrors: true
+      }
     }
   }
 })
