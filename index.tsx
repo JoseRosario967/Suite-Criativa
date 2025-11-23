e4c30fbf12a6609058d48b7343b984d366306b67
JoseRosario967 <joserosario@live.com.pt>
2025-11-23 00:05:20 +0000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <--- Esta linha é a magia que faltava!
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
